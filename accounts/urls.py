69e5a2d208add9a61a93f343f4e96d14ef3b13d5
Hamisi-Esq <brivahamisi@gmail.com>
2022-08-29 00:58:36 +0300

    Create Update & Delete (CRUD) with Model Forms

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
@@ -5,4 +6,9 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('products/', views.products, name="products"),
     path('customer/<str:pk>', views.customer, name="customer"),
+
+    path('create_order/', views.createOrder, name="create_order" ),
+    path('udate_order/<str:pk>', views.updateOrder, name="update_order" ),
+
+    path('delete_order/<str:pk>', views.deleteOrder, name="delete_order" ),
 ]
