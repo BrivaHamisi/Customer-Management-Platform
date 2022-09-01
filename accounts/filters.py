619aa8f6802ada348e943012326606e40eb1681f
Hamisi-Esq <brivahamisi@gmail.com>
2022-09-01 21:41:14 +0300

    Filter Form Table Search

--- accounts/filters.py
+++ accounts/filters.py
@@ -1,10 +1,16 @@
+from dataclasses import fields
 from pyexpat import model
+from django_filters import DateFilter, CharFilter
 import django_filters
 from .models import *
 
 class OrderFilter(django_filters.FilterSet):
+    start_date = DateFilter(field_name='date_created', lookup_expr='gte')
+    end_date = DateFilter(field_name='date_created', lookup_expr='lte')
+    note = CharFilter(field_name='note', lookup_expr='icontains')
     class Meta:
         model = Order
         fields = '__all__'
+        exclude = ['customer', 'date_created']
 
 
