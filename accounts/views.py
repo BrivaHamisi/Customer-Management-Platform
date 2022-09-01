a824904fc75947c92a7d70d8e900dbd938416797
Hamisi-Esq <brivahamisi@gmail.com>
2022-09-01 21:17:35 +0300

    Filter Search

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from .models import *
 from .forms import OrderForm
+from .filters import OrderFilter
 
 def home(request):
     orders = Order.objects.all()
@@ -29,7 +30,10 @@ def customer(request, pk):
     orders = customer.order_set.all()
     orders_count = orders.count()
 
-    context = {'customer':customer, 'orders' : orders, 'orders_count' :orders_count}
+    myFilter = OrderFilter(request.GET, queryset=orders)
+    orders = myFilter.qs
+
+    context = {'customer':customer, 'orders' : orders, 'orders_count' :orders_count, 'myFilter':myFilter}
     return render(request, 'accounts/customer.html', context)
 
 def createOrder(request, pk):
