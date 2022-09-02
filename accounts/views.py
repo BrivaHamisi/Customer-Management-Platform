0f48a61c026654e3a5f27bd6fd659ed101ae4683
Hamisi-Esq <brivahamisi@gmail.com>
2022-09-02 17:40:03 +0300

    Login Form

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,30 @@
-from multiprocessing import context
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from .models import *
 from .forms import OrderForm
 from .filters import OrderFilter
+from django.contrib.auth.forms import UserCreationForm
+
+
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form':form}
+    return render(request, 'accounts/register.html', context)
+    
+def loginPage(request):
+	context = {}
+	return render(request, 'accounts/login.html', context)
+
+# def logoutUser(request):
+# 	logout(request)
+# 	return redirect('login')
 
 def home(request):
     orders = Order.objects.all()
