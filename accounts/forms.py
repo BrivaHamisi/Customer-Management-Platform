a8e6ccacc4d5a3d1b16494ae5a56ec6b3dca42b2
Hamisi-Esq <brivahamisi@gmail.com>
2022-09-03 22:02:22 +0300

    User Creation Form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,15 @@
-from dataclasses import field
-from pyexpat import model
-from statistics import mode
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
 from .models import Order
 
 class OrderForm(ModelForm):
     class Meta:
         model = Order
         fields = '__all__'
-        
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
